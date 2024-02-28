1c675304c1f5ccc7f413bd403ff366c3919f1009
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 00:01:12 +0500

    Completed

---
+++ assingment01/question-02.ts
@@ -0,0 +1,10 @@
+// Q2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,would you like to learn some Python today?”
+
+// Solution No 02:
+
+let personName = "Muhammad Zain";
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
+
+// ***** Result *****:
+
+"Hello Muhammad Zain, would you like to learn some Python today ?;";
