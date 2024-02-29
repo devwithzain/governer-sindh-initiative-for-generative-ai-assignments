569c8bf674eca30e82d3311d32ffcd8a8b2b7f41
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 15:06:01 +0500

    Completed

---
+++ assingment01/question-20.ts
@@ -0,0 +1,45 @@
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.Write a program that creates a list containing these items.;
+
+// Solution No 20:
+
+// Creating a list of items
+let items = [
+   "Mount Everest", "K2", "Denali", "Kangchenjunga", "Mount Kilimanjaro",
+   "Nile", "Amazon", "Yangtze", "Mississippi", "Danube",
+   "United States", "Canada", "Brazil", "India", "Australia",
+   "New York City", "Tokyo", "Paris", "London", "Dubai",
+   "English", "Spanish", "Mandarin Chinese", "Hindi", "French"
+];
+
+// Printing the list of items
+for (let item of items) {
+   console.log(item);
+}
+
+// ***** Result *****:
+
+// Mount Everest;
+// K2;
+// Denali;
+// Kangchenjunga
+// Mount Kilimanjaro;
+// Nile;
+// Amazon;
+// Yangtze;
+// Mississippi;
+// Danube
+// United States;
+// Canada;
+// Brazil;
+// India;
+// Australia
+// New York City;
+// Tokyo;
+// Paris;
+// London;
+// Dubai;
+// English;
+// Spanish
+// Mandarin Chinese;
+// Hindi;
+// French;
