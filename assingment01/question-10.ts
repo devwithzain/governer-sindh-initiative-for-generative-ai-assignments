1d9fd0022b221d4dea511bd4e67557e5219c4f88
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:44:16 +0500

    Completed

---
+++ assingment01/question-10.ts
@@ -0,0 +1,18 @@
+// 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file.Then write one sentence describing what the program does.;
+
+// Amswer No 10:
+
+// Initialize a variable called favoriteNumber and store 12 as its value,
+let favoriteNumber = 12;
+// Printed favoriteNumber variable to console.
+console.log(`My favorite number is ${favoriteNumber}`);
+
+// Initialize a variable called personName and store a person name as its value,
+let student = "Muhammad Zain";
+// Printed student variable to console.
+console.log(`Hello ${student}, would you like to learn some Python today?`);
+
+// ***** Result *****
+
+// My favorite number is 12
+// Hello Muhammad Zain, would you like to learn some Python today ?;
