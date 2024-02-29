bb5ca0f30a63f6e28e6b85c1fae40ff557218902
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:42:56 +0500

    Completed

---
+++ assingment01/question-08.ts
@@ -0,0 +1,23 @@
+// Q8. You should create four lines that look like this:
+
+// _____________________________________________;
+
+// console.log(5 + 3);
+
+// _____________________________________________
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+// Solution No 08:
+
+console.log(`Addition: ${5 + 3}`);
+console.log(`Subtraction: ${12 - 4}`);
+console.log(`Multiplication: ${4 * 2}`);
+console.log(`Division: ${24 / 3}`);
+
+// ***** Result *****:
+
+// Addition: 8;
+// Subtraction: 8;
+// Multiplication: 8;
+// Division: 8;
