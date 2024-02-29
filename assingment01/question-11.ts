bea5e403b09901c76553feb1196615accb68d0f5
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:44:37 +0500

    Completed

---
+++ assingment01/question-11.ts
@@ -0,0 +1,18 @@
+// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// Solution No 11:
+
+let friednsNames = ["Rehan", "Saad", "Ebad", "Hammad", "Hamza", "Faizan"];
+
+friednsNames.map((elem) => {
+   return console.log(elem);
+});
+
+// ***** Result *****
+
+// Rehan
+// Saad;
+// Ebad;
+// Hammad;
+// Hamza;
+// Faizan;
