735b0d708a9bc7f4ec136e4ad34c45d2bb59fc0f
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:43:33 +0500

    Completed

---
+++ assingment01/question-09.ts
@@ -0,0 +1,10 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+// Solution No 09:
+
+let favoriteNumber = 12;
+console.log(`My favorite number is ${favoriteNumber}`);
+
+// ***** Result *****
+
+// My favorite number is 12
