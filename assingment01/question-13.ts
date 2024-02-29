7628d979402bd42bc5f07fc55a7a771ea2f0c205
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:47:17 +0500

    Completed

---
+++ assingment01/question-13.ts
@@ -0,0 +1,13 @@
+// 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// Solution No 13:
+
+const myFavoriteVechicles = ["Suzuki 150", "Nissan GTR R35", "Ferrari Laferrari"];
+
+console.log(`I would love to own a ${myFavoriteVechicles[0]}\n${myFavoriteVechicles[1]} It's my dream car.\nI would love to drive a${myFavoriteVechicles[2]}`);
+
+// ***** Result *****
+
+// I would love to own a Suzuki 150.
+// Nissan GTR R35 It's my dream car.
+// I would love to drive aFerrari Laferrari.;
