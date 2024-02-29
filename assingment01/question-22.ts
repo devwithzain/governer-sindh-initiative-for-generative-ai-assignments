171d54d4962a1fcd5f63888e557d32f88315b15e
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 15:06:10 +0500

    Completed

---
+++ assingment01/question-22.ts
@@ -0,0 +1,10 @@
+// 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error.Make sure you correct the error before closing the program.;
+
+// Solution No 22:
+
+let myArray = [1, 2, 3];
+console.log(myArray[5]); // This will produce an index error
+
+// ***** Result *****:
+
+// undefined
