1e0abd5d27094a960469b3c03fbf444ae65d0b96
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:29:01 +0500

    Completed

---
+++ assingment01/question-05.ts
@@ -0,0 +1,13 @@
+// Q5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+// Solution No 05:
+
+const famous_person = "Albert Einstein";
+const quote = "A person who never make a mistake never tried anything new.";
+const message = `${quote}\n${famous_person}`;
+console.log(message);
+
+// ***** Result *****
+
+// A person who never make a mistake never tried anything new.
+// Albert Einstein;
