0f8a9e00c01b54ba62206c77da8dfa9e03e02997
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:58:12 +0500

    Completed

--- assingment01/question-15.ts
+++ assingment01/question-15.ts
@@ -1,30 +1,34 @@
-// 15. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations.You’ll have to think of someone else to invite.;
+// 15. Changing Guest List: You just heard that one of your guests can’t make the;
+// dinner, so you need to send out a new set of invitations.You’ll have to think of
+// someone else to invite.
+
+// • Start with your program from Exercise 14. Add a print statement at the
+// end of your program stating the name of the guest who can’t make it.
+
+// • Modify your list, replacing the name of the guest who can’t make it with
+// the name of the new person you are inviting.
+
+// • Print a second set of invitation messages, one for each person who is still
+//    in your list.
 
 // Solution No 15:
 
-const guestList2 = ["Khursheed", "Ahsan", "Faizan"];
+let guests = ["Khursheed", "Ahsan", "Faizan"];
 
-function inviteToDinner(guest) {
-   console.log(`Dear ${guest}, I would be honored to have you join me for dinner.`);
+let cannotMakeIt = guests.pop();
+console.log(`${cannotMakeIt} can't make it to dinner.`);
+
+guests.push("Isaac Newton");
+
+for (let guest of guests) {
+   console.log(`Dear ${guest}, you are still invited to dinner.`);
 }
 
-guestList2.forEach(inviteToDinner);
-
-console.log("Unfortunately, Iqbal can't make it to dinner.");
-
-// Replace Alan Turing with a new guest
-guestList2[2] = "Junaid Alam";
-
-// Print new invitations
-guestList2.forEach(inviteToDinner);
 
 
 // ***** Result *****
 
-// Dear Khursheed, I would be honored to have you join me for dinner.
-// Dear Ahsan, I would be honored to have you join me for dinner.
-// Dear Faizan, I would be honored to have you join me for dinner.
-// Unfortunately, Iqbal can't make it to dinner.
-// Dear Khursheed, I would be honored to have you join me for dinner.
-// Dear Ahsan, I would be honored to have you join me for dinner.
-// Dear Junaid Alam, I would be honored to have you join me for dinner.
+// Faizan can't make it to dinner.
+// Dear Khursheed, you are still invited to dinner.
+// Dear Ahsan, you are still invited to dinner.
+// Dear Isaac Newton, you are still invited to dinner.;
