0196d22d7f81253acffa1268817cad1cb92e810a
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 15:06:05 +0500

    Completed

---
+++ assingment01/question-21.ts
@@ -0,0 +1,47 @@
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+// Solution No 21:
+
+// Creating objects containing items
+let mountain = {
+   name: "Mount Everest",
+   height: 8849,
+   country: "Nepal",
+   range: "Himalayas"
+};
+
+let river = {
+   name: "Nile",
+   length: 6650,
+   countries: ["Egypt", "Sudan", "Ethiopia", "Uganda", "Tanzania"]
+};
+
+let country = {
+   name: "Canada",
+   population: 38068000,
+   capital: "Ottawa"
+};
+
+// Printing the objects
+console.log(mountain);
+console.log(river);
+console.log(country);
+
+// ***** Result *****:
+
+// {
+//    name: 'Mount Everest',
+//    height: 8849,
+//    country: 'Nepal',
+//    range: 'Himalayas';
+// }
+// {
+//    name: 'Nile',
+//    length: 6650,
+//    countries: ['Egypt', 'Sudan', 'Ethiopia', 'Uganda', 'Tanzania'];
+// }
+// {
+// name: 'Canada',
+// population: 38068000,
+// capital: 'Ottawa';
+// }
