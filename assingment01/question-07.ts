95a12a8448bd1429946bddbdc0e09b9808394021
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-29 14:30:49 +0500

    Completed

---
+++ assingment01/question-07.ts
@@ -0,0 +1,13 @@
+// Q7: Number Eight: Write addition, subtraction, multiplication, and division operations that each Result in the number 8. Be sure to enclose your operations in print statements to see the Results.
+
+// Solution No 07:
+
+let number = Math.floor(Math.random() * 10);
+console.log(`Addition Operation: ${number + 8} \nSubstraction Operation: ${number - 8} \nMultiplication Operation: ${number * 8} \nDivision Operation: ${number / 8} \n`);
+
+// ***** Result *****
+
+// Addition Operation: 17
+// Substraction Operation: 1
+// Multiplication Operation: 72
+// Division Operation: 1.125;
